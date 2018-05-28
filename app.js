fd3e8bc6de3d3228944f9f2f5f56e95e0de049f6
eskelt <diegogpr3@gmail.com>
2018-05-28 22:58:58 +0200

    copia seguridad lmao

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-var express = require('express');
-var app = express();
 
-// setup ports
+var express = require('express'),
+        app = express(),
+        server = require('http').createServer(app),
+        io = require('socket.io').listen(server);
+
+server.listen(8000);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function (req,res){
-        
-        res.sendFile(__dirname + '/index.html');
 
+        res.sendFile(__dirname + '/index.html');       
+        app.use("/public", express.static(__dirname + "/public"));
+        
+
+});
+
+//Para incluir css y js
+
+
+io.sockets.on('connection', function(socket){
+        socket.on('sendMessage', function(data){
+                io.sockets.emit('newMessage', {msg: data});
+        });
 });
 
 // server listens in on port
@@ -18,4 +32,4 @@ app.listen(server_port, server_ip_address, function(){
 
         console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 
-});
+})
---
+++ public/js/msg.js~
@@ -0,0 +1,8 @@
+var socket = io.connect();
+var messageForm = document.getElementById('sendMessage');
+var message = document.getElementById('message');
+var chat = document.querySelector('article');
+
+messageForm.onsubmit=function(){
+        alert('holi');
+}
